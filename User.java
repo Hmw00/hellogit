97a53a6cfceccd52000a30f0cd7d225fb50b2382
humingwei <1287675300@qq.com>
2025-07-07 20:41:37 +0800

    定义User类

--- User.java
+++ User.java
@@ -1,1 +1,44 @@
+public class User{
+    public static void main(String[] args) {
+        // 第一行测试代码
+        Animal white = new Animal("小白", 5, "白色");
+        System.out.println(white);
+        // 第二行测试代码
+    }
+}
 
+class Animal{
+    private Stirng name;
+    private Integer age;
+    private String color;
+
+    public Animal(Stirng name, Integer age, String color) {
+        this.name = name;
+        this.age = age;
+        this.color = color;
+    }
+
+    public Stirng getName() {
+        return name;
+    }
+
+    public void setName(Stirng name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
